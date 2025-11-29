68333c775a4bf996b84a3cddb89fa859602eae7b
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-11-28 18:49:30 -0800

    add border to buttons and change background color based on selected

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from '@mui/material'
 import './footer.css'
-function Footer({choices, onClick}){
+function Footer({choices, current, onClick}){
     return(
         <div class='footer'>
             {choices.map(element => (
-                <Button key={element} onClick={()=>onClick(element)}>
+                <Button style={{color:'black', height:'74px', background:current == element?'#D9D9D9':'white', 
+                                borderRight:'2px solid black', borderRadius:0, 
+                                paddingLeft:50,paddingRight:50}} key={element} onClick={()=>onClick(element)}>
                     {element}
                 </Button>
            ) )}
