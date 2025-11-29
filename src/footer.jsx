65cc4ec0dc7c61e66e6c6c007d14d6145b931c8c
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-11-28 18:16:20 -0800

    added footer for rendering classes per semester

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,12 +1,17 @@
-
-function footer(choices, onClick){
-    
-    return 
-    (
-        <div id='root'>
-            
+import React from 'react';
+import { Button } from '@mui/material'
+import './footer.css'
+function Footer({choices, onClick}){
+    return(
+        <div class='footer'>
+            {choices.map(element => (
+                <Button key={element} onClick={()=>onClick(element)}>
+                    {element}
+                </Button>
+           ) )}
         </div>
     )
+    
 }
 
-export default footer
+export default Footer
