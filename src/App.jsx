5a645a21f554459356e83ae9dc0d96cb6d1fcf49
Adrienne Fernandez <fernandezadrienne9@gmail.com>
2025-11-28 18:16:05 -0800

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import CourseManagement from './CourseManagement'
 import { Button } from '@mui/material'
+import Footer from './footer'
 function App() {
+  const sem1 = ['test1', 'test2'];
+  const sem2 = ['test3','test4']
   const [semester, setSemester] = useState('Fall 2025');
-  const [course, setCourse] = useState('CECS 329');
+  const [course, setCourse] = useState(sem1[0]);
   const [openDialog, setOpenDialog] = useState(false);
   return (
     <div id='root'>
       <div class='header'>
         {/*add header.jsx*/}
+        {/*Testing */}
+        <p>
+          {course} 
+        </p>
       </div>
       <div class='body'>
         {/*add body.jsx*/}
       </div>
       <div class='footer'>
-        <Button style={{fontSize:'24px', color:'black'}} onClick={() => setOpenDialog(true)}>
+        <CourseManagement open={openDialog} onClose={() => setOpenDialog(false)} />
+        <Button style={{fontSize:'24px', color:'black', borderRight:'2px solid black', borderRadius:0, height:'100%'}} onClick={() => setOpenDialog(true)}>
           +
         </Button>
-        <CourseManagement open={openDialog} onClose={() => setOpenDialog(false)} />
-        
+        <Footer choices={['test1', 'test2']} onClick={setCourse}/>
       </div>
     </div>
   )
