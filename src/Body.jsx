b768c4c60c2b800b20461fa93e11d207892fdc4d
ArthurO-CSULB <arthurking.ordonez01@student.csulb.edu>
2025-11-30 13:40:15 -0800

    Initialize Dashboard Layout

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,10 @@
 import './Body.css';
 import React from 'react';
+import { Button, TextField } from '@mui/material'
 
 function Body(){
     return(
-        <div classname = "body-container">
+        <div class = "body-container">
             <AssignmentsPage />
             <GradePieChart />
             <GradeLineChart />
@@ -13,25 +14,43 @@ function Body(){
 
 function AssignmentsPage() {
     return (
-        <div classname = "assignments-page">
-            <h2>Assignments</h2>
-            {/* Add assignment components here */}
+        <div class = "assignments-page">
+            <h2 class = "assignments-header">
+                <div></div>
+                Assignments
+                <Button 
+                style ={{ fontSize: '32px', color: 'black', borderRadius: 10, height: '75%', fontWeight: 'normal' }}>
+                    +
+                </Button>
+            </h2>
+            <table class = "assignments-table">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Score</th>
+                    <th>Category</th>
+                    <th>Due Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                </table>
         </div>
     );
 }
 
 function GradePieChart() {
     return (
-        <div classname = "grade-pie-chart">
-            <h2>Grade Distribution</h2>
+        <div class = "grade-pie-chart">
+            <h2 class = "grade-pie-chart-header">Grade</h2>
         </div>
     );
 }
 
 function GradeLineChart() {
     return (
-        <div classname = "grade-line-chart">
-            <h2>Grade Over Time</h2>
+        <div class = "grade-line-chart">
+            <h2 class = "grade-line-chart-header">Grade Over Time</h2>
         </div>
     );
 } 
